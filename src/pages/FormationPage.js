fb6b7247cb904f30ce87ab3b3f05306efac56f69
Naim Abdelkerim <naimdevelopper@gmail.com>
2021-09-15 14:22:10 +0100

    formation page

--- src/pages/FormationPage.js
+++ src/pages/FormationPage.js
@@ -4,6 +4,7 @@ import { API } from "../env";
 
 import { LoadingPage } from "../components/Loading";
 import PageContent from "../components/PageContent";
+import { getImageFromApi } from "../functions";
 const FormationPage = () => {
   const [event, setevent] = useState(null);
   const { slug } = useParams();
@@ -23,7 +24,24 @@ const FormationPage = () => {
       {event == null ? (
         <LoadingPage />
       ) : (
-        <PageContent content={event.contenu} />
+        <div>
+          <div className="py-12 md:py-16 md:px-24 px-2">
+            <h1 className="text-4xl md:text-5xl font-title">{event.titre}</h1>
+            <p className="m-5">{event.description}</p>
+            <div className="gap-5">
+              {event.filiere.map((f) => (
+                <div className="border-2 rounded-lg border-gray-400 w-1/3 overflow-hidden">
+                  <img
+                    src={getImageFromApi(f.logo, "default")}
+                    alt=""
+                    className="w-full"
+                  />
+                  <h2 className="font-title text-xl md:text-2xl">{f.titre}</h2>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
